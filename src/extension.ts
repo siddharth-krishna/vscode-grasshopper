c714aab6071813c456dc0823a67c9d47134e2cc9
Siddharth Krishna <siddharth-krishna@users.noreply.github.com>
2020-03-28 09:13:48 +0000

    indent using 2 spaces instead of 4

--- src/extension.ts
+++ src/extension.ts
@@ -8,132 +8,129 @@ import * as vscode from 'vscode';
 
 
 class State {
-    public static diagnostics: vscode.DiagnosticCollection;
+  public static diagnostics: vscode.DiagnosticCollection;
 
-    public static initialize() {
-        this.diagnostics = vscode.languages.createDiagnosticCollection('Grasshopper');
-    }
+  public static initialize() {
+    this.diagnostics = vscode.languages.createDiagnosticCollection('Grasshopper');
+  }
 }
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-    State.initialize();
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('grasshopper.verifyFile', verifyFile);
+  State.initialize();
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand('grasshopper.verifyFile', verifyFile);
 
-    context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() { }
 
 function callGrasshopper(document: vscode.TextDocument, filePath: string,
-    token: vscode.CancellationToken): Promise<void> {
-    return new Promise<void>((resolve, reject) => {
-        let rawResult = '';
-        let cp = childProcess.spawn('grasshopper', [filePath], undefined)
-            .on('error', (err) => {
-                vscode.window.showErrorMessage(
-                    'Grasshopper: Failed to start subprocess.');
-                reject();
-            });
-        cp.stdout.on('data', (data: Buffer) => rawResult += data);
-        cp.stderr.on('data', (data: Buffer) => rawResult += data);
-        token.onCancellationRequested(() => cp.kill('SIGINT'));
-        cp.on('close', (code: Number, _: string) => {
-            console.log(rawResult);
-            let lines = rawResult.split('\n');
-            let errorRegex = '^File "([^"]*)", line (\\d+), columns (\\d+)-(\\d+):$';
-            let errorRegex2 =
-                '^File "([^"]*)", line (\\d+), column (\\d+) to line (\\d+), column (\\d+):$';
-            let i = 0;
-            let diags = [];
-            while (i < lines.length) {
-                let res = lines[i].match(errorRegex);
-                let res2 = lines[i].match(errorRegex2);
-                if (res) {
-                    let errorFile = res[1];  // TODO ensure this matches current file
-                    let lineNum = Number(res[2]) - 1;
-                    let startChar = Number(res[3]);
-                    let endChar = Number(res[4]);
-                    let msg = lines[++i];
+  token: vscode.CancellationToken): Promise<void> {
+  return new Promise<void>((resolve, reject) => {
+    let rawResult = '';
+    let cp = childProcess.spawn('grasshopper', [filePath], undefined)
+      .on('error', (err) => {
+        vscode.window.showErrorMessage(
+          'Grasshopper: Failed to start subprocess.');
+        reject();
+      });
+    cp.stdout.on('data', (data: Buffer) => rawResult += data);
+    cp.stderr.on('data', (data: Buffer) => rawResult += data);
+    token.onCancellationRequested(() => cp.kill('SIGINT'));
+    cp.on('close', (code: Number, _: string) => {
+      console.log(rawResult);
+      let lines = rawResult.split('\n');
+      let errorRegex = '^File "([^"]*)", line (\\d+), columns (\\d+)-(\\d+):$';
+      let errorRegex2 =
+        '^File "([^"]*)", line (\\d+), column (\\d+) to line (\\d+), column (\\d+):$';
+      let i = 0;
+      let diags = [];
+      while (i < lines.length) {
+        let res = lines[i].match(errorRegex);
+        let res2 = lines[i].match(errorRegex2);
+        if (res) {
+          let errorFile = res[1];  // TODO ensure this matches current file
+          let lineNum = Number(res[2]) - 1;
+          let startChar = Number(res[3]);
+          let endChar = Number(res[4]);
+          let msg = lines[++i];
 
-                    diags.push(new vscode.Diagnostic(
-                        new vscode.Range(new vscode.Position(lineNum, startChar),
-                            new vscode.Position(lineNum, endChar)),
-                        msg,
-                        (msg.startsWith('Error') ?
-                            vscode.DiagnosticSeverity.Error
-                            : vscode.DiagnosticSeverity.Information)
-                        // relatedInformation: [  // TODO
-                        //     new vscode.DiagnosticRelatedInformation(new vscode.Location(document.uri, new vscode.Range(new vscode.Position(1, 8), new vscode.Position(1, 9))), 'first assignment to `x`')
-                        // ]
-                    ));
-                } else if (res2) {
-                    let errorFile = res2[1];
-                    let startLine = Number(res2[2]) - 1;
-                    let startChar = Number(res2[3]);
-                    let endLine = Number(res2[4]) - 1;
-                    let endChar = Number(res2[5]);
-                    let msg = lines[++i];
+          diags.push(new vscode.Diagnostic(
+            new vscode.Range(new vscode.Position(lineNum, startChar),
+              new vscode.Position(lineNum, endChar)),
+            msg,
+            (msg.startsWith('Error') ?
+              vscode.DiagnosticSeverity.Error
+              : vscode.DiagnosticSeverity.Information)
+            // relatedInformation: [  // TODO
+            //     new vscode.DiagnosticRelatedInformation(new vscode.Location(document.uri, new vscode.Range(new vscode.Position(1, 8), new vscode.Position(1, 9))), 'first assignment to `x`')
+            // ]
+          ));
+        } else if (res2) {
+          let errorFile = res2[1];
+          let startLine = Number(res2[2]) - 1;
+          let startChar = Number(res2[3]);
+          let endLine = Number(res2[4]) - 1;
+          let endChar = Number(res2[5]);
+          let msg = lines[++i];
 
-                    diags.push(new vscode.Diagnostic(
-                        new vscode.Range(new vscode.Position(startLine, startChar),
-                            new vscode.Position(endLine, endChar)),
-                        msg,
-                        (msg.startsWith('Error') ?
-                            vscode.DiagnosticSeverity.Error
-                            : vscode.DiagnosticSeverity.Information)
-                    ));
-                } else if (lines[i].trim() !== '') {
-                    console.log("Couldn't match line:");
-                    console.log(lines[i]);
-                }
-                i++;
-            }
-            State.diagnostics.set(document.uri, diags);
-            let fileName = filePath.split('/').pop();
-            if (code === 0) {
-                vscode.window.showInformationMessage(
-                    `Grasshopper: ${fileName} verified successfully.`);
-            } else if (diags.length > 0) {
-                vscode.window.showInformationMessage(
-                    `Grasshopper: ${fileName}: ${diags.length} errors.`);
-            } else if (code === 130) {
-                vscode.window.showInformationMessage(
-                    'Grasshopper: interrupted.');
-            } else {
-                vscode.window.showErrorMessage(
-                    `Grasshopper: verification failed with code ${code}.`);
-            }
-            resolve();
-        });
-        if (!cp.pid) {
-            vscode.window.showErrorMessage(
-                'Grasshopper: Failed to start subprocess.');
-            reject();
+          diags.push(new vscode.Diagnostic(
+            new vscode.Range(new vscode.Position(startLine, startChar),
+              new vscode.Position(endLine, endChar)),
+            msg,
+            (msg.startsWith('Error') ?
+              vscode.DiagnosticSeverity.Error
+              : vscode.DiagnosticSeverity.Information)
+          ));
+        } else if (lines[i].trim() !== '') {
+          console.log("Couldn't match line:");
+          console.log(lines[i]);
         }
+        i++;
+      }
+      State.diagnostics.set(document.uri, diags);
+      let fileName = filePath.split('/').pop();
+      if (code === 0) {
+        vscode.window.showInformationMessage(
+          `Grasshopper: ${fileName} verified successfully.`);
+      } else if (diags.length > 0) {
+        vscode.window.showInformationMessage(
+          `Grasshopper: ${fileName}: ${diags.length} errors.`);
+      } else if (code === 130) {
+        vscode.window.showInformationMessage('Grasshopper: interrupted.');
+      } else {
+        vscode.window.showErrorMessage(
+          `Grasshopper: verification failed with code ${code}.`);
+      }
+      resolve();
     });
+    if (!cp.pid) {
+      vscode.window.showErrorMessage('Grasshopper: Failed to start subprocess.');
+      reject();
+    }
+  });
 }
 
 function verifyFile(): void {
-    if (vscode.window.activeTextEditor) {
-        let document = vscode.window.activeTextEditor.document;
-        // vscode.workspace.rootPath ? { cwd: vscode.workspace.rootPath } : undefined;
-        let filePath = vscode.window.activeTextEditor.document.fileName;
-        let fileName = filePath.split('/').pop();
+  if (vscode.window.activeTextEditor) {
+    let document = vscode.window.activeTextEditor.document;
+    // vscode.workspace.rootPath ? { cwd: vscode.workspace.rootPath } : undefined;
+    let filePath = vscode.window.activeTextEditor.document.fileName;
+    let fileName = filePath.split('/').pop();
 
-        // Create a progress bar notification:
-        vscode.window.withProgress({
-            location: vscode.ProgressLocation.Notification,
-            title: `Grasshopper: verifying ${fileName}...`,
-            cancellable: true
-        }, (_progress, token) => callGrasshopper(document, filePath, token));
-    } else {
-        vscode.window.showErrorMessage('Grasshopper: No active window.');
-    }
-
+    // Create a progress bar notification:
+    vscode.window.withProgress({
+      location: vscode.ProgressLocation.Notification,
+      title: `Grasshopper: verifying ${fileName}...`,
+      cancellable: true
+    }, (_progress, token) => callGrasshopper(document, filePath, token));
+  } else {
+    vscode.window.showErrorMessage('Grasshopper: No active window.');
+  }
 }
